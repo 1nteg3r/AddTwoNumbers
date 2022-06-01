c7d7f0445459d278f506b1d155fa50d809f067e4
1nteg3r <54937634+1nteg3r@users.noreply.github.com>
2022-06-01 16:01:28 +0200

    Create resenie1.py

---
+++ resenie1.py
@@ -0,0 +1,13 @@
+#prvo resnie koristejki tehnika string slicing [::-1]
+l1 = [2,4,3]
+l2 = [5,6,4]
+
+val1 = int("".join([str(i) for i in l1]))
+val2 =int("".join([str(i) for i in l2]))
+
+num = val1+val2
+
+print(f"List1 = {val1}")
+print(f"List2 = {val2}")
+print("Sobirokot na dvata broja treba da bide vo obraten redosled")
+print("Output: " + str(num)[::-1])
